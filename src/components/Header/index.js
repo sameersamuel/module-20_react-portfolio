704d3bed2cae5e7efa3fa003a1d3d4f9e0f2be87
Sameer B <sameerflyleaf@gmail.com>
2022-11-02 20:54:10 -0700

    h1 changed to Sameer

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 const Header = (props) => {
     return (
     <header className="header-container">
-        <h1>Welcome to My Portfolio</h1>
+        <h1>Welcome to Sameer's Portfolio</h1>
             <img src="https://images.unsplash.com/photo-1638025773292-1ef8c1ec8c5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2515&q=80"/>
             {props.children}
     </header>
