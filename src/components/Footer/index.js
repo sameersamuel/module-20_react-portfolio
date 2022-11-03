6f387a4d7b849444d178dc601029827e0436b06c
Sameer B <sameerflyleaf@gmail.com>
2022-11-02 19:19:42 -0700

    added current year and styling to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const Footer = () => {
@@ -15,12 +14,30 @@ const Footer = () => {
         }
     ]
  
-    
+    const LinkStyle = {
+        textDecoration: "none", 
+        color: "black", 
+        cursor: "pointer"
+    }
+
+    const FooterStyle = {
+        height: "50px",
+        display: "grid", 
+        alignItems: "center", 
+        justifyContent: "center", 
+        gridGap: "15px"
+    }
+
     return (
-        <footer>
-            {icons.map(icon => {
-                return <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer"> <i className={icon.name}></i></a>
-            })}
+        <footer style={FooterStyle}>
+            <section>
+                {new Date().getFullYear()}
+            </section>
+            <section>
+                {icons.map(icon => {
+                    return <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer" style={LinkStyle}> <i className={icon.name}></i></a>
+                })}
+            </section>
         </footer>
     )
 
